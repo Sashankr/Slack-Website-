b428d796ae952c4f1a6b32bc8ae45d37ec8236cb
Sashankr <sashankr97@gmail.com>
2021-10-12 18:02:45 +0530

    Working on edit channel feature

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const searchIcon = document.querySelector('.search-icon');
 const searchInput = document.querySelector('.search-input');
+const channelEdit = document.querySelector('.edit');
+const channelTitle = document.querySelector('.channel-title');
 
 const searchIconHide =()=>{
     searchIcon.classList.remove('show-icon');
@@ -12,3 +14,18 @@ const showSearchIcon = ()=>{
         searchIcon.classList.add('show-icon');
     }
 }
+
+const editChannel = ()=>{
+    if(channelEdit.classList.contains('fa-pen')){
+        channelEdit.classList.remove('fa-pen');
+        channelEdit.classList.add('fa-check');
+        channelTitle.disabled = false;
+        channelTitle.focus();
+        channelTitle.classList.add('title-edit');
+    }
+    else{
+        channelEdit.classList.add('fa-pen');
+        channelEdit.classList.remove('fa-check');
+    }
+}
+
